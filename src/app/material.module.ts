430228deb52b54d9c41b7e12685f4e486ca851a1
Maki <mxmcube@gmail.com>
2019-11-19 17:17:12 +0000

    Change launch button icon and text when running

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -7,6 +7,7 @@ import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
 import { MatMenuModule } from "@angular/material/menu";
 import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 
 @NgModule({
 	exports: [
@@ -18,6 +19,7 @@ import { MatProgressBarModule } from "@angular/material/progress-bar";
 		MatInputModule,
 		MatMenuModule,
 		MatProgressBarModule,
+		MatProgressSpinnerModule,
 	],
 })
 export class MaterialModule {}
