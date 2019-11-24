92650da9931dda950af2e3b874888243c6a38fc7
Maki <mxmcube@gmail.com>
2019-11-24 13:55:21 -0800

    Made sure it wouldn't spawn a second window

--- electron/main.ts
+++ electron/main.ts
@@ -29,41 +29,43 @@ if (!DEV) {
 const appLock = app.requestSingleInstanceLock();
 if (!DEV && !appLock) app.quit();
 
-const createWindow = () => {
-	if (win != null) return;
+if (appLock) {
+	const createWindow = () => {
+		if (win != null) return;
 
-	win = new BrowserWindow({
-		title: "Tivoli Cloud Launcher",
+		win = new BrowserWindow({
+			title: "Tivoli Cloud Launcher",
 
-		width: 1000,
-		height: 640,
+			width: 1000,
+			height: 640,
 
-		webPreferences: {
-			nodeIntegration: true,
-			backgroundThrottling: false,
-			nativeWindowOpen: true,
-		},
+			webPreferences: {
+				nodeIntegration: true,
+				backgroundThrottling: false,
+				nativeWindowOpen: true,
+			},
 
-		icon: APP_ICON,
-		autoHideMenuBar: true,
+			icon: APP_ICON,
+			autoHideMenuBar: true,
+		});
+
+		win.loadFile(APP_ROOT);
+
+		win.on("closed", () => {
+			win = null;
+		});
+	};
+
+	app.on("ready", createWindow);
+	app.on("activate", () => {
+		if (win == null) createWindow();
 	});
 
-	win.loadFile(APP_ROOT);
-
-	win.on("closed", () => {
-		win = null;
+	app.on("window-all-closed", () => {
+		app.quit();
 	});
-};
 
-app.on("ready", createWindow);
-app.on("activate", () => {
-	if (win == null) createWindow();
-});
-
-app.on("window-all-closed", () => {
-	app.quit();
-});
-
-app.on("second-instance", () => {
-	if (win && !DEV) if (win.isMinimized()) win.restore();
-});
+	app.on("second-instance", () => {
+		if (win && !DEV) if (win.isMinimized()) win.restore();
+	});
+}
