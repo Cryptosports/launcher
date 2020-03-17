42f525980249c7382aa11415cfb0ff9227930771
Maki <mxmcube@gmail.com>
2020-03-17 11:47:17 +0000

    Added token in develoepr tab

--- src/app/launcher/developer/developer.component.ts
+++ src/app/launcher/developer/developer.component.ts
@@ -1,14 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { SettingsService } from "../settings/settings.service";
 import { MatSlideToggleChange } from "@angular/material/slide-toggle";
+import { AuthService } from "../../auth/auth.service";
+import { take } from "rxjs/operators";
 
 @Component({
 	selector: "app-developer",
 	templateUrl: "./developer.component.html",
 	styleUrls: ["./developer.component.scss"],
 })
-export class DeveloperComponent {
-	constructor(public settingsService: SettingsService) {}
+export class DeveloperComponent implements OnInit {
+	constructor(
+		public readonly settingsService: SettingsService,
+		private readonly authService: AuthService,
+	) {}
+
+	token = "";
+
+	ngOnInit() {
+		this.authService.user$.pipe(take(1)).subscribe(user => {
+			this.token = JSON.stringify(user.token);
+		});
+	}
 
 	updateSettingChecked(key: string, event: MatSlideToggleChange) {
 		this.settingsService.setSetting(key, event.checked);
