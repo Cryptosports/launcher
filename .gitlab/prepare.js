52e7f322a886dbcf2cb63ad693dd18c9d09c0a53
Maki <mxmcube@gmail.com>
2020-03-22 19:15:06 +0000

    VC_redist.x64 is added to interface when preparing

--- .gitlab/prepare.js
+++ .gitlab/prepare.js
@@ -17,78 +17,6 @@ if (Object.values(ENV).includes(undefined)) {
 	process.exit(1);
 }
 
-function downloadFile(url, dest) {
-	return new Promise(async (resolve, reject) => {
-		const stream = fs.createWriteStream(dest);
-		const res = await fetch(url);
-		res.body.pipe(stream);
-
-		res.body.on("error", err => {
-			reject(err);
-		});
-		stream.on("finish", function() {
-			resolve();
-		});
-	});
-}
-
-async function downloadInterface() {
-	console.log(
-		"Searching for interface artifact from: " + ENV.UPSTREAM_JOBS_URL,
-	);
-	const jobs = await (
-		await fetch(ENV.UPSTREAM_JOBS_URL, {
-			headers: {
-				Authorization: "Bearer " + ENV.GITLAB_API_TOKEN,
-			},
-		})
-	).json();
-
-	// find artifact
-	let job = jobs.filter(job => job.name == ENV.UPSTREAM_JOB);
-	if (job.length == 0) {
-		console.log("Couldn't find job: \"" + ENV.UPSTREAM_JOB + '"');
-		process.exit(1);
-	}
-	job = job[0];
-
-	// download
-	const interfaceZipUrl = job.web_url + "/artifacts/download";
-	const interfaceZipPath = path.resolve(__dirname, "interface.zip");
-
-	console.log("Downloading zip from: " + interfaceZipUrl);
-	await downloadFile(interfaceZipUrl, interfaceZipPath).catch(err => {
-		console.log("Failed to download");
-		console.log(err);
-		process.exit(1);
-	});
-
-	// extract
-	const interfacePath = path.resolve(__dirname, "../interface");
-	console.log("Extracting to: " + interfacePath);
-
-	const zip = new StreamZip({
-		file: interfaceZipPath,
-	});
-
-	zip.on("ready", () => {
-		if (fs.existsSync(interfacePath))
-			fs.rmdirSync(interfacePath, { recursive: true });
-		fs.mkdirSync(interfacePath);
-
-		zip.extract("build/interface/Release", interfacePath, err => {
-			if (err) {
-				console.log("Failed to extract");
-				console.log(err);
-				process.exit(1);
-			}
-
-			zip.close();
-			console.log("Finished extracting!");
-		});
-	});
-}
-
 function updateVersion() {
 	const packagePath = path.resolve("../package.json");
 	const package = JSON.parse(fs.readFileSync(packagePath, "utf8"));
@@ -101,11 +29,112 @@ function updateVersion() {
 	);
 }
 
-try {
-	updateVersion();
-	console.log("");
-	downloadInterface();
-} catch (err) {
-	console.log(err);
-	process.exit(1);
+function downloadFile(url, dest) {
+	return new Promise(async (resolve, reject) => {
+		const stream = fs.createWriteStream(dest);
+		const res = await fetch(url);
+		res.body.pipe(stream);
+
+		res.body.on("error", error => {
+			reject(error);
+		});
+		stream.on("finish", function() {
+			resolve();
+		});
+	});
 }
+
+function downloadInterface() {
+	return new Promise(async resolve => {
+		console.log(
+			"Searching for interface artifact from: " + ENV.UPSTREAM_JOBS_URL,
+		);
+		const jobs = await (
+			await fetch(ENV.UPSTREAM_JOBS_URL, {
+				headers: {
+					Authorization: "Bearer " + ENV.GITLAB_API_TOKEN,
+				},
+			})
+		).json();
+
+		// find artifact
+		let job = jobs.filter(job => job.name == ENV.UPSTREAM_JOB);
+		if (job.length == 0)
+			throw new Error("Couldn't find job: \"" + ENV.UPSTREAM_JOB + '"');
+		job = job[0];
+
+		// download
+		const interfaceZipUrl = job.web_url + "/artifacts/download";
+		const interfaceZipPath = path.resolve(__dirname, "interface.zip");
+
+		console.log("Downloading zip from: " + interfaceZipUrl);
+		await downloadFile(interfaceZipUrl, interfaceZipPath).catch(error => {
+			throw new Error("Failed to download");
+		});
+
+		// extract
+		const interfacePath = path.resolve(__dirname, "../interface");
+		console.log("Extracting to: " + interfacePath);
+
+		const zip = new StreamZip({
+			file: interfaceZipPath,
+		});
+
+		zip.on("ready", () => {
+			if (fs.existsSync(interfacePath))
+				fs.rmdirSync(interfacePath, { recursive: true });
+			fs.mkdirSync(interfacePath);
+
+			zip.extract("build/interface/Release", interfacePath, error => {
+				if (error) throw new Error(error);
+
+				zip.close();
+				console.log("Finished extracting!");
+
+				resolve();
+			});
+		});
+	});
+}
+
+async function patchInterface() {
+	return new Promise(resolve => {
+		const interfacePath = path.resolve(__dirname, "../interface");
+		console.log("Patching interface: " + interfacePath);
+
+		const zip = new StreamZip({
+			file: path.resolve(__dirname, "VC_redist.x64.zip"),
+		});
+
+		zip.on("ready", () => {
+			if (!fs.existsSync(interfacePath)) fs.mkdirSync(interfacePath);
+
+			zip.extract(null, interfacePath, error => {
+				if (error) throw new Error(error);
+
+				const fileNames = Object.keys(zip.entries());
+				console.log(
+					fileNames.map(name => "Extracted: " + name).join("\n"),
+				);
+
+				zip.close();
+
+				console.log("Finished patching!");
+				resolve();
+			});
+		});
+	});
+}
+
+(async () => {
+	try {
+		updateVersion();
+		console.log("");
+		await downloadInterface();
+		console.log("");
+		await patchInterface();
+	} catch (error) {
+		console.log(error);
+		process.exit(1);
+	}
+})();
